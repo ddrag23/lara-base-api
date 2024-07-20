8dc5d57413f1c8a10d1cedd1f6c042f7ee8efb0a
ddrag23 <kurodyami@gmail.com>
2024-07-21 05:45:44 +0700

    add allowed field for search

--- app/Http/Repositories/UserRepository.php
+++ app/Http/Repositories/UserRepository.php
@@ -10,6 +10,7 @@ class UserRepository extends BaseRepository
     protected array $relation = [];
     protected array $detailRelation = [];
     protected array $countRelation = [];
+    protected array $allowedFilter = ['name', 'email'];
     function __construct()
     {
         $this->model = new User();
